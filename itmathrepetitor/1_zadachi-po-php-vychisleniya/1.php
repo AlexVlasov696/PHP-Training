7833dc5e86fad81f8411afc556131d931bf5bb8a
AlexVlasov696 <vlasovblogs@yandex.ru>
2016-06-12 13:48:09 +0300

    first commit

---
+++ itmathrepetitor/1_zadachi-po-php-vychisleniya/1.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lera
+ * Date: 12.06.2016
+ * Time: 10:37
+ */
+/**
+ * Created by PhpStorm.
+ * User: Lera
+ * Date: 12.06.2016
+ * Time: 10:20
+ */
+$x = 5;
+$y = 8;
+
+$sum = $x + $y;
+$proiz = $x * $y;
+
+echo $sum."<br />";
+echo $proiz;
+
