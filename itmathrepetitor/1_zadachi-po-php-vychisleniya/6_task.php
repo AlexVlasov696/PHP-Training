11609d007ba5fee2c523105e31cce11c7d99b444
AlexVlasov696 <vlasovblogs@yandex.ru>
2016-06-14 14:05:37 +0300

    Novie zada4i

---
+++ itmathrepetitor/1_zadachi-po-php-vychisleniya/6_task.php
@@ -0,0 +1,13 @@
+<?php
+/**Дано натуральное числа. Найдите остатки от деления этих чисел на 3 и на 5.**/
+
+$z = 12;
+
+
+$ostatok3 = $z % 3;
+
+
+$ostatok5 = $z % 5;
+
+echo "Dano chislo <b>12</b><br />";
+echo "Vot ostatok on delenia <b>12</b> na <b>3</b>: ".$ostatok3.", a eto ostatok ot delenia <b>12</b> na <b>5</b>: ".$ostatok5;
